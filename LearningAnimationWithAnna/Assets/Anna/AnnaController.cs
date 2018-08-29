6ee231943438462b3075c5879a07b7709764ff82
Q-Flash <mubarakdcricketer@hotmail.com>
2018-08-29 05:51:12 -0400

    Improved Animations and more
    1) Added some skill cast duration so you cannot kick and move at the same time  2) Removed trees to prevent lag
    3) Moved kick to right click

--- LearningAnimationWithAnna/Assets/Anna/AnnaController.cs
+++ LearningAnimationWithAnna/Assets/Anna/AnnaController.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class AnnaController : MonoBehaviour {
 
 	static Animator anim;
-	public float speed = 10.0F;
-	public float rotationSpeed = 100.0F;
+	private bool isCasting = false; //Bool to signal when a skill is being casted
+	private float castingTimeout = 0.00F; //How much time is left for cast duration of current skill
+	public float speed = 10.0F; //Running speed
+	public float rotationSpeed = 100.0F; //Turn speed
+
+	//Skill cast times
+	private float kickCastDuration = 1.833F; //Cast time for kick skill
 
 
 	// Use this for initialization
@@ -15,23 +20,54 @@ public class AnnaController : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
+		//Update skill cast durations
+		if (castingTimeout > 0.00F) {
+			castingTimeout -= Time.deltaTime;
+		} else {
+			isCasting = false;
+			castingTimeout = 0.00F;
+		}	
 
-		float translation = Input.GetAxis("Vertical")*speed;
-		float rotation = Input.GetAxis("Horizontal")*rotationSpeed;
-		translation *= Time.deltaTime;
-		rotation *= Time.deltaTime;
-		transform.Translate(0, 0, translation);
-		transform.Rotate(0, rotation, 0);
+
+		//Movement with arrow-keys/WASD
+		float translation = 0;
+		float rotation = 0;
+		
+		if (!isCasting) {
+			translation = Input.GetAxis ("Vertical") * speed;
+			rotation = Input.GetAxis ("Horizontal") * rotationSpeed;
+			translation *= Time.deltaTime;
+			rotation *= Time.deltaTime;
+			transform.Translate (0, 0, translation);
+			transform.Rotate (0, rotation, 0);
+		}
+
 
 		//Setting trigger for jumping when spacebar is pressed
-		if(Input.GetButtonDown("Jump")){
-			anim.SetTrigger("isJumping");
+		if (Input.GetButtonDown ("Jump")) {
+			if (isCasting) {
+				
+				
+			} else {
+				anim.SetTrigger("isJumping");
+			}
+		
+			
 		}
 
 		//Kick when M is pressed
 		if(Input.GetButtonDown("Kick")){
-			anim.SetTrigger("isKicking");
+			if (isCasting) {
+				
+				
+			} else {
+				anim.SetTrigger("isKicking");
+				isCasting = true; 
+				castingTimeout = kickCastDuration;
+			}
+			
 		}
 
 		//Set running animation when character is running
